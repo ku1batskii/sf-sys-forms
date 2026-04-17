fbf3dd23ba701b158aae30e01f13fc008217e1ba
ku1batskii <168655448+ku1batskii@users.noreply.github.com>
2026-04-17 15:49:33 +0300

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,9 +1,15 @@
+import { initializeApp } from "firebase/app";
+import { getDatabase } from "firebase/database";
+
 const firebaseConfig = {
-  apiKey: "AIzaSy...",
+  apiKey: "AIzaSyCWzyEL0_bB7tkFs_KEDctcw7iNwiYR0mw",
   authDomain: "sf-sys-forms.firebaseapp.com",
   databaseURL: "https://sf-sys-forms-default-rtdb.europe-west1.firebasedatabase.app",
   projectId: "sf-sys-forms",
-  storageBucket: "sf-sys-forms.appspot.com",
-  messagingSenderId: "...",
-  appId: "..."
+  storageBucket: "sf-sys-forms.firebasestorage.app",
+  messagingSenderId: "649493071442",
+  appId: "1:649493071442:web:a622ab7fd8025268aec18b"
 };
+
+const app = initializeApp(firebaseConfig);
+export const db = getDatabase(app);
