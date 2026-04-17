76e7fd81beb22c347df8099ac2dec6fc5f053f4e
ku1batskii <168655448+ku1batskii@users.noreply.github.com>
2026-04-17 16:00:28 +0300

    Update hosting.jsx

--- src/pages/hosting.jsx
+++ src/pages/hosting.jsx
@@ -1,7 +1,4 @@
-import Questionnaire from '../components/Questionnaire'
-
-const BLOCKS = [
-  import Questionnaire from ‘../components/Questionnaire’
+import Questionnaire from ‘../components/Questionnaire’
 
 const BLOCKS = [
 {
@@ -223,18 +220,4 @@ criticalIds={CRITICAL_IDS}
 dbPath="hosting"
 />
 )
-}
-]
-
-const CRITICAL_IDS = ["2.3", "2.4", "4.1", "5.1", "5.3"]
-
-export default function Hosting() {
-  return (
-    <Questionnaire
-      title="Хостинг-платформа с автодеплоем"
-      blocks={BLOCKS}
-      criticalIds={CRITICAL_IDS}
-      dbPath="hosting"
-    />
-  )
 }
