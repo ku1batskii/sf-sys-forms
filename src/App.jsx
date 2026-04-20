ff4d5962600aad3c4157006244be77badfe31ff7
ku1batskii <168655448+ku1batskii@users.noreply.github.com>
2026-04-20 16:05:03 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
-import Hosting from './pages/hosting'
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom"
+import Hosting from "./pages/hosting"
+import LiderKrovlya from "./pages/lider-krovlya"
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/hosting" element={<Hosting />} />
-        {/* НОВЫЕ СТРАНИЦЫ ДОБАВЛЯЕШЬ ЗДЕСЬ: */}
-        {/* <Route path="/office-it" element={<OfficeIT />} /> */}
+        <Route path="/lider-krovlya" element={<LiderKrovlya />} />
+        {/* НОВЫЕ СТРАНИЦЫ ДОБАВЛЯЕШЬ ЗДЕСЬ */}
         <Route path="*" element={<Navigate to="/hosting" />} />
       </Routes>
     </BrowserRouter>
