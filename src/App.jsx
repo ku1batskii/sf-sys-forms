b89ea004293ef9e3f119299f1abc9636d1892407
ku1batskii <168655448+ku1batskii@users.noreply.github.com>
2026-05-26 14:16:56 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom"
 import Hosting from "./pages/hosting"
 import LiderKrovlya from "./pages/lider-krovlya"
+import Avtopark from "./pages/avtopark"
 
 export default function App() {
   return (
@@ -8,6 +9,7 @@ export default function App() {
       <Routes>
         <Route path="/hosting" element={<Hosting />} />
         <Route path="/lider-krovlya" element={<LiderKrovlya />} />
+        <Route path="/avtopark" element={<Avtopark />} />
         {/* НОВЫЕ СТРАНИЦЫ ДОБАВЛЯЕШЬ ЗДЕСЬ */}
         <Route path="*" element={<Navigate to="/hosting" />} />
       </Routes>
